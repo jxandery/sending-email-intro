73d833cdb6f0057bc4544c9b08d17dbf60d849d5
Jack Yeh <jack.yeh@gmail.com>
2015-07-09 10:04:53 -0600

    create test:logged in user sees email form

--- test/integration/user_can_send_email_test.rb
+++ test/integration/user_can_send_email_test.rb
@@ -12,6 +12,20 @@ class UserCanSendEmailTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Welcome")
   end
 
+  def test_logged_in_user_sees_email_form
+    user = User.create(name: "Horace",
+                       email: "horace@turing.io",
+                       password: "password")
 
+    visit '/'
+    click_on "Login"
+    fill_in "email", with: "horace@turing.io"
+    fill_in "password", with: "password"
+    click_on "Submit"
+
+    assert_equal '/', current_path
+    assert page.has_content?("Horace")
+    assert page.has_css?("form#email-form")
+  end
 
 end
